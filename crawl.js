7bcf60a4447af58713382a39f03798e0ad3dc444
Ryan Muendesi <muendesitawana@gmail.com>
2026-01-29 14:29:28 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+    console.log(`Actively crawling: ${currentURL}`);
+
+    try {
+        const response = await fetch(currentURL);
+
+        if (response.status > 399) {
+            console.log(`error in fetch with status code: ${response.status} on page: ${currentURL}`);
+            return;
+        }
+
+        const contentType = response.headers.get("content-type");
+        if(!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+        }
+
+        console.log(await response.text());
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+    
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -38,4 +61,5 @@ function normalizeURL(urlstring) {
 module.exports = {
     normalizeURL,
     getURLsFromHTML,
+    crawlPage,
 }
