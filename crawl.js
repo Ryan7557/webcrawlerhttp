5c06a8e55ba2e84e76926e1a48da3f6ba32ca997
Ryan Muendesi <muendesitawana@gmail.com>
2026-01-29 15:22:05 +0200

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,22 +1,43 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL ,currentURL, pages) {
+    const baseURLObject = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if (baseURLObject.hostname !== currentURLObj.hostname) {
+        return pages;
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
     console.log(`Actively crawling: ${currentURL}`);
 
     try {
         const response = await fetch(currentURL);
-
         if (response.status > 399) {
             console.log(`error in fetch with status code: ${response.status} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
         const contentType = response.headers.get("content-type");
         if(!contentType.includes("text/html")) {
-            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
+            return pages;
         }
 
-        console.log(await response.text());
+        const htmlBody = await response.text();
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages);
+        }
+        return pages;
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
     }
